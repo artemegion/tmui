f86fbeaef1f9a13c6dddcabe3d55147cd49cf2be
Paweł Ignasiak <p.ignasiak26@gmail.com>
2023-01-30 01:07:58 +0100

    removed some trash comments

--- Tmui/Immediate/TextBoxScrollFlags.cs
+++ Tmui/Immediate/TextBoxScrollFlags.cs
@@ -1,16 +1,4 @@
-﻿// TODO:
-// + ScrollView
-// + Dropdown
-// + SelectBox
-// + Button checkbox
-// + TextBox built-in scrolling
-//   + no vertical align with scrolling
-// + Checkbox with label overload
-// + Docs
-// + comments explaining code
-// + refactor common code
-
-namespace Tmui.Immediate;
+﻿namespace Tmui.Immediate;
 
 [Flags]
 public enum TextBoxScrollFlags
